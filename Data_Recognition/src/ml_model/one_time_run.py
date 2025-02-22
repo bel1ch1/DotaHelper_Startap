b585e0401a755c5be4fb87cdb32e390b3ef7a6cb
bel1c1 <andreyzv5555@gmail.com>
2025-02-22 15:34:05 +0300

    for the presentation

---
+++ Data_Recognition/src/ml_model/one_time_run.py
@@ -0,0 +1,49 @@
+from ultralytics import YOLO
+import cv2
+
+# Модель
+model_path = 'C:/Mirea_Projects/DotaHelper_Startap/Data_Recognition/src/ml_model/best_2.pt'
+model = YOLO(model_path)
+
+# Обработка изображения
+image_path = 'C:\Mirea_Projects\DotaHelper_Startap\Data_Recognition\src\ml_model\combined_screenshot_1740225874.png'
+image = cv2.imread(image_path)
+resized_image = cv2.resize(image, (600, 440))
+
+results = model(resized_image)
+
+confidence_threshold = 0.8  # Порог уверенности
+object_counter = 1          # Счетчик объектов
+detected_objects = {}
+
+# Обработка результатов
+for result in results:
+    boxes = result.boxes  # Bounding boxes
+    for box in boxes:
+        class_id = int(box.cls)  # Класс
+        confidence = float(box.conf)  # Уверенность
+        if confidence >= confidence_threshold:
+            class_name = model.names[class_id]  # Название класса
+            # Сохраняем сопоставление: номер -> (класс, уверенность)
+            detected_objects[object_counter] = (class_name, confidence)
+
+            # Получение координат bounding box
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+            # Рисование контуров
+            cv2.rectangle(resized_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            label = f"{object_counter}"
+            cv2.putText(resized_image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+            object_counter += 1
+
+
+if detected_objects:
+    for number, (class_name, confidence) in detected_objects.items():
+        print(f"{number}: {class_name}, Confidence: {confidence:.2f}")
+else:
+    print("Ничего не распознано.")
+
+cv2.imshow("Detected Objects", resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
