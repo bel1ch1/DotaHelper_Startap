5776db73235e25cc6f12b7f7c4b050354317a4ee
bel1ch1 <andreyzv5555@gmail.com>
2025-03-23 13:45:10 +0300

    first stage of praser

--- Data_From_Api/parse_match_data/stratz_match_parser.py
+++ Data_From_Api/parse_match_data/stratz_match_parser.py
@@ -1,101 +1,104 @@
 import json
 
-# Загрузка JSON-файла
-with open("match_data.json", "r", encoding="utf-8") as f:
+
+# Загрузка JSON
+path_to_data = 'match_data.json'
+with open(path_to_data, 'r') as f:
     data = json.load(f)
 
-# Основные данные матча
-match_data = {
-    "didRadiantWin": data["data"]["match"]["didRadiantWin"],
-    "durationSeconds": data["data"]["match"]["durationSeconds"],
-    "radiantKills": data["data"]["match"]["radiantKills"],
-    "direKills": data["data"]["match"]["direKills"],
-}
+
+#####################  Parser  ####################################################################
+class MatchParser:
+    def __init__(self, json_data):
+        """
+        Инициализация класса с загрузкой JSON данных.
+        """
+        self.data = json_data
+        self.players = self.data.get('data', {}).get('match', {}).get('players', [])
+        self.radiantKills = self.data.get('data', {}).get('match', {}).get('radiantKills', [])
+        self.direKills = self.data.get('data', {}).get('match', {}).get('direKills', [])
+        self.didRadiantWin = self.data.get('data', {}).get('match', {}).get('didRadiantWin', False)
+        self.durationSeconds = self.data.get('data', {}).get('match', {}).get('durationSeconds', 0)
+        self.radiant_heroes = []
+        self.dire_heroes = []
+        self.position_order = {
+            "POSITION_1": 1,
+            "POSITION_2": 2,
+            "POSITION_3": 3,
+            "POSITION_4": 4,
+            "POSITION_5": 5
+        }
 
 
-# Данные игроков
-players_data = []
-for player in data["data"]["match"]["players"]:
-    player_info = {
-        "isRadiant": player["isRadiant"],
-        "heroId": player["heroId"],
-        "heroShortName": player["hero"]["shortName"],
-        "position": player["position"],
-    }
+    def process_players(self):
+        """
+        Разделение игроков на команды Radiant и Dire.
+        """
+        # Создаем списки героев и сразу сортируем их по позициям
+        self.radiant_heroes = sorted(
+            [
+                {"shortName": player["hero"]["shortName"], "heroId": player['heroId'], "position": player["position"]}
+                for player in self.players
+                if player.get("isRadiant", False)
+            ],
+            key=lambda x: self.position_order[x["position"]]
+        )
 
-    # События игрока
-    playback_data = player["playbackData"]
-    for event_type, events in playback_data.items():
-        if events:  # Если есть события
-            for event in events:
-                event_data = {**player_info, "event_type": event_type, **event}
-                players_data.append(event_data)
-
-# Обработка inventoryEvents
-inventory_events = []
-for player in match_data.get("players", []):
-    player_info = {
-        "isRadiant": player.get("isRadiant"),
-        "heroId": player.get("heroId"),
-        "heroShortName": player.get("hero", {}).get("shortName"),
-        "position": player.get("position"),
-    }
-
-    for event in player.get("playbackData", {}).get("inventoryEvents", []):
-        event_data = {**player_info, "time": event.get("time")}
-
-        # Обработка item0-item5
-        for i in range(6):
-            item_key = f"item{i}"
-            item_data = event.get(item_key)
-            event_data[item_key] = item_data.get("itemId") if item_data else None
-
-        # Обработка backPack0-backPack2
-        for i in range(3):
-            backpack_key = f"backPack{i}"
-            backpack_data = event.get(backpack_key)
-            event_data[backpack_key] = backpack_data.get("itemId") if backpack_data else None
-
-        # Обработка neutral0
-        neutral_data = event.get("neutral0")
-        event_data["neutral0"] = neutral_data.get("itemId") if neutral_data else None
-
-        inventory_events.append(event_data)
-        print(event_data)
+        self.dire_heroes = sorted(
+            [
+                {"shortName": player["hero"]["shortName"], "heroId": player['heroId'], "position": player["position"]}
+                for player in self.players
+                if not player.get("isRadiant", True)
+            ],
+            key=lambda x: self.position_order[x["position"]]
+        )
 
 
-spirit_bear_events = []
-for player in match_data.get("players", []):
-    player_info = {
-        "isRadiant": player.get("isRadiant"),
-        "heroId": player.get("heroId"),
-        "heroShortName": player.get("hero", {}).get("shortName"),
-        "position": player.get("position"),
-    }
-
-    for event in player.get("playbackData", {}).get("spiritBearInventoryEvents", []):
-        event_data = {**player_info, "time": event.get("time")}
-
-        # Обработка item0-item5
-        for i in range(6):
-            item_key = f"item{i}"
-            item_data = event.get(item_key)
-            event_data[item_key] = item_data.get("itemId") if item_data else None
-
-        # Обработка backPack0-backPack2
-        for i in range(3):
-            backpack_key = f"backPack{i}"
-            backpack_data = event.get(backpack_key)
-            event_data[backpack_key] = backpack_data.get("itemId") if backpack_data else None
-
-        # Обработка neutral0
-        neutral_data = event.get("neutral0")
-        event_data["neutral0"] = neutral_data.get("itemId") if neutral_data else None
-
-        spirit_bear_events.append(event_data)
+    def get_match_data(self):
+        '''
+        Получение данных:
+        Кто победил: [0]
+        Продолжительность матча в секундах: [1]
+        '''
+        if self.didRadiantWin is True:
+            win = 'Radiant'
+        else:
+            win = 'Dire'
+        return [win, self.durationSeconds]
 
 
-print(match_data)
-print('============================================================================================')
-print(players_data[0])
-print(inventory_events)
+    def get_sorted_heroes(self):
+        """
+        Получение отсортированных списков героев для обеих команд:
+        radiant: [0]
+        dire: [1]
+        """
+        return self.radiant_heroes, self.dire_heroes
+
+
+    def get_kills_advantage_per_minute(self, radiant, dire):
+        """
+        Метод вычисляет разницу по фрагам за каждую минуту матча.
+        """
+        status = []
+        for i in range(len(radiant)):
+            res = radiant[i] - dire[i]
+            status.append(res)
+        return status
+###################################################################################################
+
+
+parser = MatchParser(data)
+parser.process_players()
+
+# Пример использования
+match_data = parser.get_match_data()
+print("Match Data:", match_data)
+print('===========================================')
+radiant_heroes, dire_heroes = parser.get_sorted_heroes()
+print("Radiant Heroes:", radiant_heroes)
+print('===========================================')
+print("Dire Heroes:", dire_heroes)
+print('===========================================')
+kills_advantage = parser.get_kills_advantage_per_minute(parser.radiantKills, parser.direKills)
+print("Kills Advantage:", kills_advantage)
