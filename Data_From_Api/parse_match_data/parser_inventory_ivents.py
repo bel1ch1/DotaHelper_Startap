973abf038ecdcf9b0853ff3f853ef02e987b70a6
bel1ch1 <andreyzv5555@gmail.com>
2025-03-28 21:41:56 +0300

    add new  parse logic

--- Data_From_Api/parse_match_data/parser_inventory_ivents.py
+++ Data_From_Api/parse_match_data/parser_inventory_ivents.py
@@ -10,7 +10,20 @@ class NewMatchParser:
         self.radiantKills = self.data.get('data', {}).get('match', {}).get('radiantKills', [])
         self.direKills = self.data.get('data', {}).get('match', {}).get('direKills', [])
 
-        # List of IDs allowed opponent items
+        # Обновленный список разрешенных ID предметов пользователя (только ключи)
+        self.allowed_player_item_ids = {
+            1, 2, 6, 7, 11, 12, 13, 14, 15, 16, 20, 25, 26, 27, 28, 29, 30, 31, 32, 34,
+            36, 37, 38, 39, 40, 41, 42, 48, 50, 56, 63, 65, 73, 75, 77, 79, 81, 88, 90,
+            92, 94, 96, 98, 100, 102, 104, 108, 110, 112, 114, 116, 119, 121, 123, 125,
+            127, 133, 135, 137, 139, 141, 143, 145, 147, 149, 151, 152, 154, 156, 158,
+            160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 181, 185, 190, 196,
+            206, 208, 210, 214, 216, 220, 223, 225, 226, 229, 231, 232, 235, 236, 237,
+            242, 244, 247, 249, 250, 252, 254, 256, 259, 261, 263, 265, 267, 269, 273,
+            277, 473, 534, 596, 598, 600, 603, 604, 609, 610, 635, 692, 911, 931, 939,
+            964, 1097, 1107, 1123, 1128, 1466, 1575, 1806
+        }
+
+        # Список разрешенных ID предметов противников (оставьте ваш текущий)
         self.allowed_item_ids = [
             37, 40, 96, 98, 100, 102, 104, 119, 127, 139,
             152, 156, 160, 176, 204, 206, 208, 210, 249,
@@ -29,7 +42,6 @@ class NewMatchParser:
 
         self.hero_items = {}
         self.process_inventory()
-
 
     def get_winner(self):
         """
@@ -103,7 +115,7 @@ class NewMatchParser:
 
 
     def extract_items_from_event(self, event):
-        """ Extract items from inventory events"""
+        """Извлекает предметы из события инвентаря с фильтрацией"""
         items = set()
         if not event:
             return items
@@ -113,20 +125,21 @@ class NewMatchParser:
             slot_data = event.get(slot)
             if slot_data and isinstance(slot_data, dict):
                 item_id = slot_data.get('itemId')
-                if item_id:
+                # Проверяем принадлежность к разрешенным предметам
+                if item_id is not None and item_id in self.allowed_player_item_ids:
                     items.add(item_id)
         return items
 
 
     def get_player_items(self, hero_id):
-        """Возвращает предметы игрока по этапам"""
+        """Возвращает ID предметов игрока по этапам (только разрешенные)"""
         if hero_id not in self.hero_items:
             return {}
 
         return {
-            group: list(items)
+            group: sorted(list(items))  # Сортировка ID для удобства
             for group, items in self.hero_items[hero_id]['items'].items()
-            if items
+            if items  # Исключаем пустые группы
         }
 
 
@@ -288,7 +301,7 @@ parser = NewMatchParser(new_data)
 # Printing all heroes
 parser.print_all_heroes()
 
-hero_id = int(input("\nВведите ID героя пользователя: "))
+hero_id = 82 #int(input("\nВведите ID героя пользователя: "))
 
 winner = parser.get_winner()
 print(f"\nПобедитель матча: {winner}")
@@ -312,7 +325,150 @@ for group, items in player_items.items():
     print(f"{group}: {items}")
 
 # enemy items
-enemy_items = parser.get_filtered_enemy_items(hero_id)
-print("\nОтфильтрованные предметы противников по этапам:")
-for group, items in enemy_items.items():
+player_items = parser.get_player_items(hero_id)
+print("Player items by stage (IDs only):")
+for group, items in player_items.items():
     print(f"{group}: {items}")
+
+
+# items_ids = {
+#     0:"Blink Dagger",
+#     1:"Blades of Attack",
+#     2:"Helm of Iron Will",
+#     3:"Javelin",
+#     4:"Quelling Blade",
+#     5:"Ring of Protection",
+#     6:"Gauntlets of Strength",
+#     7:"Slippers of Agility",
+#     8:"Mantle of Intelligence",
+#     9:"Iron Branch",
+#     10:"Circlet",
+#     11:"Gloves of Haste",
+#     12:"Morbid Mask",
+#     13:"Ring of Regen",
+#     14:"Sage's Mask",
+#     15:"Boots of Speed",
+#     16:"Gem of True Sight",
+#     17:"Cloak",
+#     18:"Talisman of Evasion",
+#     19:"Magic Stick",
+#     20:"Magic Wand",
+#     21:"Ghost Scepter",
+#     22:"Clarity",
+#     23:"Healing Salve",
+#     24:"Dust of Appearance",
+#     25:"Bottle",
+#     26:"Tango",
+#     27:"Boots of Travel",
+#     28:"Phase Boots",
+#     29:"Ring of Health",
+#     30:"Power Treads",
+#     31:"Hand of Midas",
+#     32:"Bracer",
+#     33:"Wraith Band",
+#     34:"Null Talisman",
+#     35:"Mekansm",
+#     36:"Vladmir's Offering",
+#     37:"Ring of Basilius",
+#     38:"Pipe of Insight",
+#     39:"Urn of Shadows",
+#     40:"Headdress",
+#     41:"Scythe of Vyse",
+#     42:"Orchid Malevolence",
+#     43:"Eul's Scepter of Divinity",
+#     44:"Force Staff",
+#     45:"Dagon",
+#     46:"Aghanim's Scepter",
+#     47:"Refresher Orb",
+#     48:"Assault Cuirass",
+#     49:"Heart of Tarrasque",
+#     50:"Black King Bar",
+#     51:"Shiva's Guard",
+#     52:"Bloodstone",
+#     53:"Linken's Sphere",
+#     54:"Vanguard",
+#     55:"Blade Mail",
+#     56:"Divine Rapier",
+#     57:"Monkey King Bar",
+#     58:"Radiance",
+#     59:"Butterfly",
+#     60:"Daedalus",
+#     61:"Skull Basher",
+#     62:"Battle Fury",
+#     63:"Manta Style",
+#     64:"Crystalys",
+#     65:"Armlet of Mordiggian",
+#     66:"Shadow Blade",
+#     67:"Sange and Yasha",
+#     68:"Satanic",
+#     69:"Mjollnir",
+#     70:"Eye of Skadi",
+#     71:"Sange",
+#     72:"Helm of the Dominator",
+#     73:"Maelstrom",
+#     74:"Desolator",
+#     75:"Yasha",
+#     76:"Mask of Madness",
+#     77:"Diffusal Blade",
+#     78:"Ethereal Blade",
+#     79:"Soul Ring",
+#     80:"Arcane Boots",
+#     81:"Orb of Venom",
+#     82:"Drum of Endurance",
+#     83:"Veil of Discord",
+#     84:"Diffusal Blade (level 2)",
+#     85:"Rod of Atos",
+#     86:"Abyssal Blade",
+#     87:"Heaven's Halberd",
+#     88:"Tranquil Boots",
+#     89:"Enchanted Mango",
+#     90:"Boots of Travel 2",
+#     91:"Meteor Hammer",
+#     92:"Nullifier",
+#     93:"Lotus Orb",
+#     94:"Solar Crest",
+#     95:"Guardian Greaves",
+#     96:"Aether Lens",
+#     97:"Octarine Core",
+#     98:"Dragon Lance",
+#     99:"Faerie Fire",
+#     100:"Crimson Guard",
+#     101:"Wind Lace",
+#     102:"Moon Shard",
+#     103:"Silver Edge",
+#     104:"Bloodthorn",
+#     105:"Echo Sabre",
+#     106:"Glimmer Cape",
+#     107:"Aeon Disk",
+#     108:"Kaya",
+#     109:"Crown",
+#     110:"Hurricane Pike",
+#     111:"Infused Raindrops",
+#     112:"Spirit Vessel",
+#     113:"Holy Locket",
+#     114:"Kaya and Sange",
+#     115:"Yasha and Kaya",
+#     116:"Voodoo Mask",
+#     117:"Witch Blade",
+#     118:"Falcon Blade",
+#     119:"Mage Slayer",
+#     120:"Overwhelming Blink",
+#     121:"Swift Blink",
+#     123:"Arcane Blink",
+#     124:"Aghanim's Shard",
+#     125:"Wind Waker",
+#     126:"Helm of the Overlord",
+#     127:"Eternal Shroud",
+#     128:"Revenant's Brooch",
+#     129:"Boots of Bearing",
+#     130:"Harpoon",
+#     131:"Diffusal Blade",
+#     132:"Disperser",
+#     133:"Phylactery",
+#     134:"Blood Grenade",
+#     135:"Pavise",
+#     136:"Gleipnir",
+#     137:"Orb of Frost",
+#     138:"Parasma",
+#     139:"Khanda"
+# }
